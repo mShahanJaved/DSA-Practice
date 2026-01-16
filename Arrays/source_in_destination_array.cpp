3bc4e7e374599b1c727e36670a2abd262e96a9f0
Shahan Javed <sshahann31@gmail.com>
2026-01-16 22:59:42 +0500

    Create source_in_destination_array.cpp

---
+++ Arrays/source_in_destination_array.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+	char ch1[5] = { 'a', 'b', 'c', 'd', 'e' };
+	char ch2[5];
+	int j = 0;
+	for (int i=0; i<5; i++)
+	{
+		ch2[j] = ch1[i];
+		cout << ch2[j] << " ";
+		j++;
+	}
+	
+
+
+	return 0;
+}
