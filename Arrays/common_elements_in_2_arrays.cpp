ff29306253c371431f69cc8f0c00bf71b32c4c16
Shahan Javed <sshahann31@gmail.com>
2026-01-27 09:51:35 +0500

    Update common_elements_in_2_arrays.cpp

--- Arrays/common_elements_in_2_arrays.cpp
+++ Arrays/common_elements_in_2_arrays.cpp
@@ -2,59 +2,55 @@
 using namespace std;
 
 int main() {
-    int arr1[4];
-    int arr2[4];
-
-    
-    cout << "Enter Array 1:" << endl;
-    for (int i = 0; i < 4; i++) 
+    int arr1[100];
+    int size1;
+    cout << "Enter Size1: ";
+    cin >> size1;
+    for (int i = 0; i < size1; i++)
     {
-        cout << "Element " << i << ": ";
         cin >> arr1[i];
     }
-
-   
-    cout << "Enter Array 2:" << endl;
-    for (int i = 0; i < 4; i++) 
+    int arr2[100];
+    int size2;
+    cout << "Enter Size2: ";
+    cin >> size2;
+    for (int i = 0; i < size2; i++)
     {
-        cout << "Element " << i << ": ";
         cin >> arr2[i];
     }
-
-    cout << "Common Elements: ";
-
-   
-    for (int i = 0; i < 4; i++) 
+    int common[100];
+    int commonSize = 0;
+    for (int i = 0; i< size1; i++)
     {
-
-       
-        bool already_printed = false;
-        for (int k = 0; k < i; k++) 
+        for (int j = 0; j<size2; j++)
         {
-            if (arr1[i] == arr1[k]) 
+            
+            if (arr1[i] == arr2[j])
             {
-                already_printed = true;
-                break;
-            }
-        }
+                bool alreadyExists = false;
+                for (int k = 0; k < commonSize; k++)
+                {
+                    if (common[k] == arr1[i])
+                    {
+                        alreadyExists = true;
+                        break;
+                    }
+                }
 
-        
-        if (already_printed)
-        {
-            continue;
-        }
 
-       
-        for (int j = 0; j < 4; j++) 
-        {
-            if (arr1[i] == arr2[j]) 
-            {
-                cout << arr1[i] << " ";
-                break; 
+                if (!alreadyExists)
+                {
+                    common[commonSize] = arr1[i];
+                    commonSize++;
+                }
             }
         }
     }
-    cout << endl;
 
+  
+    for (int i = 0; i < commonSize; i++)
+    {
+        cout << common[i] << " ";
+    }
     return 0;
 }
